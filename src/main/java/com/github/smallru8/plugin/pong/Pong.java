cdbf8b7c036eb2bcfa442bd385e67b6966946fbd
WIN-06VB3MHE2U4\Administrator <smallru80923@gmail.com>
2021-10-29 11:34:02 +0800

    Update NikoBot API to 3.6

--- src/main/java/com/github/smallru8/plugin/pong/Pong.java
+++ src/main/java/com/github/smallru8/plugin/pong/Pong.java
@@ -10,31 +10,31 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import com.github.smallru8.NikoBot.Core;
 import com.github.smallru8.NikoBot.Embed;
 import com.github.smallru8.NikoBot.event.Event.MessageEvent;
 import com.github.smallru8.NikoBot.plugins.PluginsInterface;
 
 public class Pong implements PluginsInterface{
 
+	private String pluginName = "PingPong";
+	
 	public void onDisable() {
-		// TODO Auto-generated method stub
 		EventBus.getDefault().unregister(this);
 	}
 
 	public void onEnable() {
-		// TODO Auto-generated method stub
 		EventBus.getDefault().register(this);
 	}
 	@Subscribe(threadMode = ThreadMode.MAIN)
 	public void onMessageRecved(MessageEvent e) {
-		if(e.msg.getContentRaw().startsWith("/ping")&&!e.msg.getAuthor().isBot()) {
+		if(Core.pluginsMGR.isAllowedGuild(e.event.getGuild().getId(), pluginName)&&e.msg.getContentRaw().startsWith("/ping")&&!e.msg.getAuthor().isBot()) {
 			long ms = e.msg.getTimeCreated().until(LocalDateTime.now().atOffset(ZoneId.systemDefault().getRules().getOffset(Instant.now())), ChronoUnit.MILLIS);
 			Embed.EmbedSender(Color.pink, e.msg.getChannel(), "Pong!:ping_pong:"+ms+"ms", "");
 		}
 	}
 	public String pluginsName() {
-		// TODO Auto-generated method stub
-		return "PingPong";
+		return pluginName;
 	}
 
 }
